95a9ebe13fcf9dd9cf3191681d324f9ff948b56f
Kim-Coding <ptibabmgo@gmail.com>
2023-01-19 09:19:11 +0900

    Initial Commit

---
+++ recommendNumber/recommendNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  recommendNumberApp.swift
+//  recommendNumber
+//
+//  Created by 김기훈 on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct recommendNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
