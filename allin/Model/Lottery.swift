f8501ccc20ccdbef6450c1b29c2243f8eb37b0ff
Kim-Coding <ptibabmgo@gmail.com>
2023-03-24 13:42:10 +0900

    refactor: lottery model 수정

--- allin/Model/Lottery.swift
+++ allin/Model/Lottery.swift
@@ -8,34 +8,15 @@
 import Foundation
 
 struct Lottery: Codable {
-    let drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6, bnusNo :Int
+    var drwtNo1: Int = 0
+    var drwtNo2: Int = 0
+    var drwtNo3: Int = 0
+    var drwtNo4: Int = 0
+    var drwtNo5: Int = 0
+    var drwtNo6: Int = 0
+    var bnusNo: Int = 0
     
-#if os(watchOS)
-    static func getArray(_ string: String) -> [Int] {
-        var temp = string
-        temp.removeFirst()
-        temp.removeLast()
-        
-        let array = temp.components(separatedBy: ",")
-        
-        var result = Array(repeating: 0, count: 7)
-        
-        for line in array {
-            let split = line.components(separatedBy: ":")
-            
-            switch split[0] {
-            case "\"drwtNo1\"": result[0] = Int(split[1])!
-            case "\"drwtNo2\"": result[1] = Int(split[1])!
-            case "\"drwtNo3\"": result[2] = Int(split[1])!
-            case "\"drwtNo4\"": result[3] = Int(split[1])!
-            case "\"drwtNo5\"": result[4] = Int(split[1])!
-            case "\"drwtNo6\"": result[5] = Int(split[1])!
-            case "\"bnusNo\"": result[6] = Int(split[1])!
-            default: continue
-            }
-        }
-        
-        return result
+    func winNumbers() -> [Int] {
+        return [drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6]
     }
-#endif
 }
