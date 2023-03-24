bb294d150a124d0279f1af207a90680b5357035c
Kim-Coding <ptibabmgo@gmail.com>
2023-03-24 19:35:40 +0900

    refactor: manager 제거

---
+++ allin/Error.swift
@@ -0,0 +1,16 @@
+//
+//  Error.swift
+//  allin
+//
+//  Created by 김기훈 on 2023/03/24.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case urlError
+}
+
+enum FileError: Error {
+    case bundleError
+}
